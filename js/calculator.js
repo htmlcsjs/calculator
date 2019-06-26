a11247fbb7dbb92a30be72b8656493d74ec7844f
htmlcsjs <lennyhall2006@icloud.com>
2019-06-26 18:31:09 +0100

    fixed a fex things and added equlas and cler tags

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,21 @@
 var commandStr = "";
+var clearNext = false
 $(".input").on("click", function() {
+  if (clearNext == true) {
+    commandStr = "<span class=\"gray\">Enter text below</span>"
+    $("#textHolder").html(commandStr);
+  }
+
   var inputText = $(this).text();
-   commandStr += inputText
-  $("#textHolder").text(string)
+  commandStr += inputText;
+  $("#textHolder").text(commandStr);
+});
+$(".clear").on("click", function() {
+  commandStr = ""
+  $("#textHolder").text(commandStr);
+});
+$(".equals").on("click", function() {
+
+  commandStr = inputText;
+  $("#textHolder").text(commandStr);
 });
